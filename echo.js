b8cedc0f0aafbe96e40ff4423570d198aa419ac8
G. Dalezios <georgedalezios@hotmail.com>
2024-06-07 21:34:45 +0300

    Updates

---
+++ echo.js
@@ -0,0 +1,7 @@
+function printText() {
+    const inputDOM = document.getElementById('inputText')
+    const inputText = inputDOM.value
+
+    const parDOM = document.getElementById('helloText')
+    parDOM.innerHTML = inputText
+}
---
+++ hello-event.js
@@ -0,0 +1,9 @@
+function printText() {
+    // const inputDOM = document.getElementById('inputText')
+    // const inputText = inputDOM.value
+    const inputText = document.getElementById('inputText').value
+
+    // const parDOM = document.getElementById('helloText')
+    // parDOM.innerHTML = inputText
+    document.getElementById('helloText').innerHTML = inputText
+}
