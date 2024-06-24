4dcf8904f3030c2be26f70ff10517d000ce5f66a
G. Dalezios <georgedalezios@hotmail.com>
2024-06-24 20:12:40 +0300

    Background changer app

---
+++ background-changer-app/main.js
@@ -0,0 +1,40 @@
+let color = undefined
+
+this.addEventListener('DOMContentLoaded', function() {
+    this.document.querySelector('#btn').addEventListener('click', function() {
+        onClickMeClicked()
+    })
+})
+
+/**
+ * Handler. Actions taken after 'Click Me' is clicked.
+ */
+function onClickMeClicked() {
+    updateBg()
+}
+
+/**
+ * Updates the state and the UI.
+ */
+function updateBg() {
+    color = getColor()
+    showBgColor()
+}
+
+/**
+ * Selects randomly a color from a standard array
+ * 
+ * @returns the color
+ */
+function getColor() {
+    const colors = ['black', 'red', 'green', 'blue', 'white']
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
+/**
+ * 
+ */
+function showBgColor() {
+    document.querySelector('#color').innerHTML = color
+    document.body.style.backgroundColor = color
+}
