05b5f5fbb677b2c769390ffe87ffb213181134f5
G. Dalezios <georgedalezios@hotmail.com>
2024-09-23 20:23:14 +0300

    updates & new examples

--- node1/myapp1/index.js
+++ node1/myapp1/index.js
@@ -16,6 +16,29 @@ app.get('/login', (req, res) => {
     res.send("This is login page");
 })
 
+app.get('/user', (req, res) => {
+
+    let query = req.query;
+    console.log(query);
+
+    let name = query.name;
+    let surname = query.surname;
+    let age = query.age;
+
+    res.send("Name: " + name + " Surname: " + surname + " Age: " + age);
+})
+
+app.get('/details/:name/:surname/:age', (req, res) => {
+    let params = req.params;
+    console.log(params);
+
+    let name = params.name;
+    let surname = params.surname;
+    let age = params.age;
+
+    res.send("Name: " + name + " Surname: " + surname + " Age: " + age);
+})
+
 app.listen(port, () => {
     console.log("Server is up");
 })
